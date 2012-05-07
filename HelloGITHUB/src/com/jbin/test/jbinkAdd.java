a237f6609ef590865cf3513de2f33a6a5fda155b
jbink17@gmail.com <jbink17@gmail.com>
2012-05-07 11:20:37 +0900

    jbink Add src/jbinkAdd Class Create!!

---
+++ HelloGITHUB/src/com/jbin/test/jbinkAdd.java
@@ -0,0 +1,13 @@
+package com.jbin.test;
+
+public class jbinkAdd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, I'm jbin!!");
+	}
+
+}
